507c7421a92a77f622e25f25b509b4378c8fdeaa
Pedro Barquin Ayuso <pedro.barquin@gmail.com>
2017-09-06 23:06:25 +0200

    up

--- instalacion.sh
+++ instalacion.sh
@@ -1,18 +1,25 @@
-mkdir /home/pi/Desktop/Imagenes
-mkdir /home/pi/Desktop/Grabaciones
-mv Interfaz /home/pi/Desktop
-mv Diccionario /home/pi
-mkdir /home/pi/pocketsphinx
+sudo mkdir /home/pi/Desktop/Imagenes
+sudo mkdir /home/pi/Desktop/Grabaciones
+sudo mkdir /home/pi/Desktop/Capturas
+sudo cp -r Interfaz /home/pi/Desktop
+sudo cp -r Diccionario /home/pi
+sudo mkdir /home/pi/pocketsphinx
 cd /home/pi/pocketsphinx
 
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-sudo echo "sudo python /home/pi/Desktop/Interfaz/menu.py" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo echo "@xset s off" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo echo "@xset -dpms" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo echo "@xset s noblank" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo echo "sleep(5)" >> /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo echo "sudo python /home/pi/Desktop/Interfaz/lanzar.py &" >> /home/pi/.config/lxsession/LXDE-pi/autostart
 
 sudo apt-get install ppp wvdial -y
 sudo apt-get install hostapd isc-dhcp-server -y
 sudo apt-get install python-serial -y
+
+sudo apt-get install mplayer -y
 
 sudo apt-get install python-pygame -y
 
@@ -30,16 +37,19 @@ sudo apt-get install lame -y
 sudo apt-get install festival -y
 sudo apt-get install espeak -y
 
-wget https://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz/download -O sphinxbase.tar.gz
-wget https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz/download -O pocketsphinx.tar.gz
+sudo wget https://sourceforge.net/projects/cmusphinx/files/sphinxbase/5prealpha/sphinxbase-5prealpha.tar.gz/download -O sphinxbase.tar.gz
+sudo wget https://sourceforge.net/projects/cmusphinx/files/pocketsphinx/5prealpha/pocketsphinx-5prealpha.tar.gz/download -O pocketsphinx.tar.gz
+
+sudo tar -xzvf sphinxbase.tar.gz
+sudo tar -xzvf pocketsphinx.tar.gz
 
 cd sphinxbase-5prealpha
-./configure --enable-fixed
+sudo ./configure --enable-fixed
 sudo make
 sudo make install
 cd ../pocketsphinx-5prealpha
-./configuresudo 
-make
+sudo ./configure
+sudo make
 sudo make install
 
 sudo ldconfig
